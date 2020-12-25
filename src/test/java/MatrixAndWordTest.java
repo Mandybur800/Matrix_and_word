39d0fdefc5158360d48145ac37f2ed3b04a911bd
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-25 16:48:52 +0200

    added 2 tests and checkstyle for them

--- src/test/java/MatrixAndWordTest.java
+++ src/test/java/MatrixAndWordTest.java
@@ -1,14 +1,19 @@
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class MatrixAndWordTest {
     @Test
     public void checkMatrix_notOk() {
-        assertThrows(RuntimeException.class,() -> MatrixAndWord.getCoordinate("HEDYHRWF", "RED"));
-        assertThrows(RuntimeException.class,() -> MatrixAndWord.getCoordinate("HEDHYHRWF", "RED"));
-        assertThrows(RuntimeException.class,() -> MatrixAndWord.getCoordinate("HEgjYtRjF", ""));
-        assertThrows(RuntimeException.class,() -> MatrixAndWord.getCoordinate("EQBNEeASg", "ANANAS"));
+        assertThrows(RuntimeException.class, () ->
+                MatrixAndWord.getCoordinate("HEDYHRWF", "RED"));
+        assertThrows(RuntimeException.class, () ->
+                MatrixAndWord.getCoordinate("HEDHYHRWF", "RED"));
+        assertThrows(RuntimeException.class, () ->
+                MatrixAndWord.getCoordinate("HEgjYtRjF", ""));
+        assertThrows(RuntimeException.class, () ->
+                MatrixAndWord.getCoordinate("EQBNEeASg", "ANANAS"));
     }
 
     @Test
@@ -16,18 +21,29 @@ public class MatrixAndWordTest {
         assertEquals("[0,2]", MatrixAndWord.getCoordinate("QGDREGVDG", "D"));
         assertEquals("[0,2]->[1,2]", MatrixAndWord.getCoordinate("EQBGEeABg", "BE"));
         assertEquals("[1,0]->[1,1]->[2,1]", MatrixAndWord.getCoordinate("QGDREGVDG", "RED"));
-        assertEquals("[1,2]->[1,3]->[0,3]->[0,2]", MatrixAndWord.getCoordinate("QLGNAEKIRLRNGEAE", "KING"));
-        assertEquals("[0,0]->[1,0]->[1,1]->[2,1]->[2,2]", MatrixAndWord.getCoordinate("GYNOERENIGHEDjtGURNWLDJVI", "GReED"));
-        assertEquals("[2,2]->[2,1]->[3,1]->[4,1]->[4,2]", MatrixAndWord.getCoordinate("NFRKFNNAFKARFRNKAKAKNNKKF", "Frank"));
+        assertEquals("[1,2]->[1,3]->[0,3]->[0,2]",
+                MatrixAndWord.getCoordinate("QLGNAEKIRLRNGEAE", "KING"));
+        assertEquals("[0,0]->[1,0]->[1,1]->[2,1]->[2,2]",
+                MatrixAndWord.getCoordinate("GYNOERENIGHEDjtGURNWLDJVI", "GReED"));
+        assertEquals("[2,2]->[2,1]->[3,1]->[4,1]->[4,2]",
+                MatrixAndWord.getCoordinate("NFRKFNNAFKARFRNKAKAKNNKKF", "Frank"));
+        assertEquals("[1,1]->[1,2]->[2,2]->[2,1]->[2,0]",
+                MatrixAndWord.getCoordinate("UKJVXNAPBXELPLHVNLDKBVVNM", "APPLE"));
+        assertEquals("[0,2]->[1,2]->[2,2]->[2,3]->[2,4]->[3,4]->[4,4]->"
+                        + "[5,4]->[5,3]->[5,2]->[5,1]->[4,1]->[4,0]->[5,0]",
+                MatrixAndWord.getCoordinate("FBDHBAAGNITISTDASABIDDBITILBNILALASGTATIGIYGNTGND",
+                        "DISABILITATING"));
     }
 
     @Test
     public void checkForRepeat_Ok() {
-        assertEquals("[2,3]->[2,2]->[2,1]->[1,1]->[0,1]->[0,0]", MatrixAndWord.getCoordinate("SATURYNFLKGANADEYJNJJLLAD", "ANANAS"));
+        assertEquals("[2,3]->[2,2]->[2,1]->[1,1]->[0,1]->[0,0]",
+                MatrixAndWord.getCoordinate("SATURYNFLKGANADEYJNJJLLAD", "ANANAS"));
     }
 
     @Test
     public void checkPalindrome_Ok() {
-        assertEquals("[0,2]->[0,3]->[1,3]->[2,3]->[2,2]->[2,1]->[3,1]", MatrixAndWord.getCoordinate("HXROZBDZTZNOTAGhRuhBNDJMM", "ROTATOR"));
+        assertEquals("[0,2]->[0,3]->[1,3]->[2,3]->[2,2]->[2,1]->[3,1]",
+                MatrixAndWord.getCoordinate("HXROZBDZTZNOTAGhRuhBNDJMM", "ROTATOR"));
     }
 }
